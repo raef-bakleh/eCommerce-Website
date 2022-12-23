449cc987b2f2291743757c90d930053f4db49df1
Raef <bakleh.raef@gmail.com>
2022-12-23 03:33:04 +0100

    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cfae0a5c99c90b2b"; }
+/******/ 		__webpack_require__.h = function() { return "a5fdc270acc2a340"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React from "react";
 import { AiFillInstagram } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p> All rights reserved</p>
+      <p> Copyright © 2022. All rights reserved.</p>
+      <p>
+        Developed by{" "}
+        <Link href={"https://github.com/raef-bakleh"} target="_blank">
+          Raef Bakleh
+        </Link>
+      </p>
       <p className="icons">
         <AiFillInstagram />
       </p>
